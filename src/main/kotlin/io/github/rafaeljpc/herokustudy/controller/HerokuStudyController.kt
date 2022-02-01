83a06d964eb1cde37adec5aff8794b2fdd022d5e
Rafael Correia <rafaeljpc@gmail.com>
2022-01-31 21:47:37 -0300

    controller

---
+++ src/main/kotlin/io/github/rafaeljpc/herokustudy/controller/HerokuStudyController.kt
@@ -0,0 +1,11 @@
+package io.github.rafaeljpc.herokustudy.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HerokuStudyController {
+
+    @GetMapping("/")
+    fun home() = "Hello World!!"
+}
